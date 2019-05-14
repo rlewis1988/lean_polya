afc3f61142dd35e5ef1893046812436a7f961759
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2019-05-14 10:47:41 +0200

    clean up tests

--- src/mul_tests.lean
+++ src/mul_tests.lean
@@ -1,16 +1,12 @@
 import .control
+
 open polya tactic
 variables x y z u v w  r : ℚ
---set_option profiler true
---set_option trace.app_builder true
 
-set_option profiler true
-
-
+#exit
 
 example (h1 : u > 0) (h2 : u < 1*v) (h3 : z > 0) (h4 : 1*z + 1*1 < 1*w) (h5 : rat.pow (1*u + 1*v + 1*z) 3 ≥ 1* rat.pow (1*u + 1*v + 1*w + 1*1) 5) : false :=
 by  polya h1 h2 h3 h4 h5
-
 
 example (h1 : u > 1*1) (h2 : u < 1*v) (h3 : rat.pow u 15 > 1*rat.pow v 17) : false :=
 by polya h1 h2 h3 
@@ -20,13 +16,10 @@ by polya h1 h2 h3
 by polya h1 h2 h3
 -/
 
-
 example (h1 : x > 0) (h2 : x < 1*1) (h3 : rat.pow x 2 ≥ 1*x) : false :=
 by  polya h1 h2 h3
 
-
 /-
-
 0 \leq n, \; n < (K / 2)x, \; 0 < C, \; 0 < \varepsilon < 1 \myRightarrow \left(1 + \frac{\varepsilon}{3 (C + 3)}\right) \cdot n < K x
 -/
 section
@@ -35,7 +28,6 @@ example (h1 : 0 ≤ n) (h2 : n < (1/2)*K*x) (h3 : 0 < C) (h4 : 0 < eps) (h5 : ep
 by polya h1 h2 h3 h4 h5 h6
 
 end
-
 
 /-
 Tests from the old Polya repository.
@@ -90,11 +82,10 @@ by polya h1 h2 h3 h4 h5 h6 h7-/
 
 local infix `**`:1000 := rat.pow
 
-#exit
 -- line 309
 example (h1 : x + rat.pow y (-1) < 2) (h2 : y < 0) (h3 : y * rat.pow x (-1) > 1) (h4 : -2 ≤ x) (h5 : x < 2) (h6 : -2 ≤ y) (h7 : y ≤ 2) (h8 : rat.pow x 2 * rat.pow y (-1) > 1 - x) : false :=
 by polya h1 h2 h3 h4 h5 h6 h7 h8
-#exit
+
 -- line 314
 example (h1 : 0 < u) (h2 : u < v) (h3 : 1 < x) (h4 : x < y) (h5 : 0 < w) (h6 : w < z) (h7 : u + x*w < v+(rat.pow y 2)*z) : false :=
 by polya h1 h2 h3 h4 h5 h6 h7
@@ -152,22 +143,14 @@ example  (h1 : x > 0) (h2 : x < 1*1) (h3 : rat.pow (1*1 + (-1)*x) (-1) ≤ 1*(ra
 by 
 polya h1 h2 h3
 
-
-
-
 example (h1 : x > 1*1) (h1' : y ≥ 0) (h2 : (1 + y)*x < 1*(1 + y)) : false :=
 by polya  h1 h1' h2 
-
-
 
 example (e1 : x > 0) /-(e2' : x*y > 0)-/ (e2' : y > 0) (e2 : x > 1*y) (e3 : z > 1*x) (e3' : z > 0) (e4 : y > 1*z) : false :=
 by polya e1 e2' e2 e3 e3' e4
 
 example (e1 : x > 0) (e2 : y > 0) (e2' : z > 0) (e3 : y > 1*z) (e4 : x > 1*z) (e5 : x*y<1*(rat.pow z 2))  : false :=
 by polya e1 e2 e2' e3 e4 e5 
-
-
-#exit
 
 /-set_option trace.app_builder true
 example (h0 : (1 : ℚ) > 0) (h1 : x > 1*1) (h1' : y ≥ 0) (h2 : (1 + y)*x < 1*(1 + y)) : false :=
@@ -207,11 +190,6 @@ trace $ ("contr found", bb.contr_found),
 pf ← bb.contr.reconstruct,
 apply pf-/
 
-#exit
-
-set_option trace.app_builder true
---set_option pp.implicit true
---set_option pp.numerals false
 -- left off with : bug at prod_form.to_expr??
 example (e1 : x > 0) /-(e2' : x*y > 0)-/ (e2' : y > 0) (e2 : x > 1*y) (e3 : z > 1*x) (e3' : z > 0) (e4 : y > 1*z) : false :=
 by do 
@@ -228,10 +206,6 @@ trace $ ("contr found", bb.contr_found),
 pf ← bb.contr.reconstruct,
 apply pf
 
-
-
-#exit
-
 example (e1 : x > 0) (e2 : y > 0) (e2' : z > 0) (e3 : y > 1*z) (e4 : x > 1*z) (e5 : x*y<1*(rat.pow z 2)) (e6 : rat.pow z 2 > 0) (e7 : x*y > 0) : false :=
 by do 
 exps ← monad.mapm get_local [`e1, `e2, `e2', `e3, `e4, `e5, `e6, `e7],
@@ -240,7 +214,6 @@ bb.trace_exprs,
 --(l, _) ← return $ get_add_defs bb,
 --trace l,
 (_, bb) ← return $ prod_form.add_new_ineqs bb,
-
 --bb.trace,
 trace $ ("contr found", bb.contr_found),
 pf ← bb.contr.reconstruct,
