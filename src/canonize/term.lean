3ce928832dc80bf10082ec1b787f2669a78fa1fe
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2019-05-16 16:59:38 +0200

    add step datatype

--- src/canonize/term.lean
+++ src/canonize/term.lean
@@ -41,7 +41,7 @@ instance : decidable_rel lt := by delta lt; apply_instance
 
 --def dict (α : Type) := ℕ → α
 structure dict (α : Type*) := (map : ℕ → α)
-variables {α : Type*} [discrete_field α] (eval : dict α)
+variables {α : Type*} [discrete_field α]
 
 def eval_term (map : ℕ → α) : term → α
 | zero := 0
@@ -56,6 +56,7 @@ def eval_term (map : ℕ → α) : term → α
 instance to_eval : has_coe_to_fun (dict α) := ⟨λ _, term → α, eval_term ∘ dict.map⟩
 
 section
+variable {eval : dict α}
 variables {x y : term} {i : ℕ} {c : ℚ} {n : ℤ}
 @[simp] lemma eval_zero : eval zero = 0 := rfl
 @[simp] lemma eval_one : eval one = 1 := rfl
@@ -67,6 +68,7 @@ variables {x y : term} {i : ℕ} {c : ℚ} {n : ℤ}
 end
 
 section
+variable {eval : dict α}
 variables (x y z : term) (i : ℕ) (c : ℚ) (n : ℤ)
 theorem mul_zero : eval (sca x 0) = 0 := by simp
 theorem add_comm : eval (add x y) = eval (add y x) := add_comm _ _
@@ -74,17 +76,20 @@ theorem add_comm : eval (add x y) = eval (add y x) := add_comm _ _
 end
 
 --TODO: find a name
-def foo (x y : term) : Type :=
+def foo (eval : dict α) (x y : term) : Type :=
 {cs : list term // (∀ c ∈ cs, eval c ≠ 0) → eval x = eval y}
 
 namespace foo
 
-def of_eq {x y : term} (h : eval x = eval y) : foo eval x y :=
+variable (eval : dict α)
+variables {x y z : term}
+
+def of_eq (h : eval x = eval y) : foo eval x y :=
 ⟨[], λ _, h⟩
 
-def rfl (x : term) : foo eval x x := of_eq _ (congr_arg _ rfl)
+def rfl : foo eval x x := of_eq _ (congr_arg _ rfl)
 
-def trans (x y z : term) (u : foo eval x y) (v : foo eval y z) : foo eval x z :=
+def trans (u : foo eval x y) (v : foo eval y z) : foo eval x z :=
 ⟨list.union u.val v.val,
     assume H, eq.trans
     (u.property $ assume c hc, H _ (list.mem_union_left hc _))
@@ -93,6 +98,24 @@ def trans (x y z : term) (u : foo eval x y) (v : foo eval y z) : foo eval x z :=
 
 end foo
 
-def canonize (x : term) : Σ (y : term), foo eval x y := ⟨x, foo.rfl eval x⟩
+def step (eval : dict α) : Type := Π (x : term), Σ (y : term), foo eval x y
+
+namespace step
+
+def comp {eval : dict α} (f g : step eval) : step eval :=
+assume x : term,
+let ⟨y, pr1⟩ := g x in
+let ⟨z, pr2⟩ := f y in
+⟨z, foo.trans _ pr1 pr2⟩
+
+infixr ` ∘ ` := comp
+
+end step
+
+variable {eval : dict α}
+def f : step eval := sorry
+def g : step eval := sorry
+
+def canonize : step eval := f ∘ g
 
 end term
