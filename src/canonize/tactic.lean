7cf362e30535dfd5d474ea8136be9325b76061b1
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2019-05-17 17:50:17 +0200

    revert to list, faster on small examples

--- src/canonize/tactic.lean
+++ src/canonize/tactic.lean
@@ -2,11 +2,6 @@ import .term data.list.alist data.finmap
 
 namespace polya
 open term tactic
-
-def list.to_dict {α} [inhabited α] (l : list α) : dict α :=
-⟨λ i, list.func.get i l.reverse⟩
---TODO: more efficient implementation
-
 open native
 
 meta structure cache_ty :=
@@ -15,7 +10,8 @@ meta structure cache_ty :=
 (val : tactic expr)
 
 private meta def empty_val : tactic expr :=
-to_expr ``(@has_emptyc.emptyc (finmap (λ _ : ℕ, ℝ)) _)
+to_expr ``([] : list ℝ)
+--to_expr ``(@has_emptyc.emptyc (finmap (λ _ : ℕ, ℝ)) _)
 meta instance : has_emptyc cache_ty := ⟨⟨0, rb_map.mk _ _, empty_val⟩⟩
 
 meta def state_dict : Type → Type := state cache_ty
@@ -25,7 +21,8 @@ meta instance state_dict_monad_state : monad_state cache_ty state_dict := state_
 
 meta def insert_val (k : ℕ) (e : expr) (m : expr) : tactic expr :=
 do
-mk_app `finmap.insert [reflect k, e, m]
+mk_app `list.cons [e, m]
+--mk_app `finmap.insert [reflect k, e, m]
 
 meta def get_atom (e : expr) : state_dict ℕ :=
 get >>= λ s,
@@ -37,13 +34,18 @@ match s.atoms.find e with
     return i
 end
 
+def list.to_dict {α} [inhabited α] (l : list α) : dict α :=
+⟨λ i, list.func.get i l.reverse⟩
+--TODO: more efficient implementation
+
 def finmap.to_dict (m : finmap (λ _ : ℕ, ℝ)) : dict ℝ :=
 ⟨λ i, match finmap.lookup i m with (some x) := x | _ := 0 end⟩
 
 meta def cache_ty.get_dict (s : cache_ty) : tactic expr :=
 do
     m ← s.val,
-    mk_app ``finmap.to_dict [m]
+    mk_app ``list.to_dict [m]
+    --mk_app ``finmap.to_dict [m]
 
 meta def term_of_expr : expr → state_dict term
 | `(0 : ℝ) := return zero 
