6208789be38e16ca8d786e19683d71f4cc2750b8
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2019-05-28 21:27:18 +0200

    ugly proof

--- src/canonize/pterm.lean
+++ src/canonize/pterm.lean
@@ -8,18 +8,22 @@ variables {α : Type} [discrete_field α]
 variables {γ : Type} [coeff γ]
 variables [morph γ α] {ρ : dict α}
 
+--@[derive decidable_eq]
 structure xterm : Type :=
 (term : @nterm γ _)
 (exp : znum)
 
 namespace xterm
 
+--TODO: unable to derive decidable_eq
+instance decidable_eq : decidable_eq (@xterm γ _) := sorry
+
 def le (x y : @xterm γ _) : Prop := x.term ≤ y.term
 
 instance : has_le (@xterm γ _) := ⟨le⟩
 instance : decidable_rel (@xterm.le γ _) := by delta xterm.le; apply_instance
 
-def pow (x : @xterm γ _) (n : znum) : @xterm γ _ :=
+def mul (x : @xterm γ _) (n : znum) : @xterm γ _ :=
 ⟨x.term, x.exp * n⟩
 
 def of_nterm (x : @nterm γ _) : @xterm γ _ :=
@@ -36,6 +40,35 @@ x.term.eval ρ ^ (x.exp : ℤ)
 theorem eval_def {x : @nterm γ _} {n : znum} :
   eval ρ ⟨x, n⟩ = x.eval ρ ^ (n : ℤ) :=
 rfl
+
+theorem eval_add' {x : @nterm γ _} {n m : znum} :
+  nterm.eval ρ x ≠ 0 → xterm.eval ρ ⟨x, n + m⟩ = xterm.eval ρ ⟨x, n⟩ * xterm.eval ρ ⟨x, m⟩ :=
+begin
+  intro hx,
+  simp [xterm.eval, fpow_add hx]
+end
+
+theorem eval_add (x : @nterm γ _) (n m : znum) :
+  n + m ≠ 0 → xterm.eval ρ ⟨x, n + m⟩ = xterm.eval ρ ⟨x, n⟩ * xterm.eval ρ ⟨x, m⟩ :=
+begin
+  intro h1,
+  have h2 : n ≠ 0 ∨ m ≠ 0, by { sorry },
+  by_cases hx : nterm.eval ρ x = 0,
+  { simp only [eval], rw hx,
+    apply eq.trans,
+    { apply zero_fpow,
+      rw ← znum.cast_zero,
+      exact_mod_cast h1 },
+    { rw zero_eq_mul,
+      cases h2,
+      { left, apply zero_fpow,
+        rw ← znum.cast_zero,
+        exact_mod_cast h2 },
+      { right, apply zero_fpow,
+        rw ← znum.cast_zero,
+        exact_mod_cast h2 }}},
+  { simp [eval, fpow_add, hx] }
+end
 
 theorem eval_to_nterm (x : @xterm γ _) :
   x.to_nterm.eval ρ = x.eval ρ :=
@@ -55,9 +88,9 @@ begin
   simp [eval_to_nterm]
 end
 
-theorem eval_pow {x : @xterm γ _} {n : znum} :
-  (x.pow n).eval ρ = x.eval ρ ^ (n : ℤ) :=
-by simp [pow, eval, fpow_mul] 
+theorem eval_mul {x : @xterm γ _} {n : znum} :
+  (x.mul n).eval ρ = x.eval ρ ^ (n : ℤ) :=
+by simp [mul, eval, fpow_mul] 
 
 end xterm
 
@@ -66,41 +99,42 @@ structure pterm : Type :=
 
 namespace pterm
 
-def append (S1 S2 : @pterm γ _) : @pterm γ _ :=
-⟨S1.terms ++ S2.terms⟩
+def append (P1 P2 : @pterm γ _) : @pterm γ _ :=
+⟨P1.terms ++ P2.terms⟩
 
 instance : has_append (@pterm γ _) := ⟨append⟩
 
-theorem append_terms (S1 S2 : @pterm γ _) :
-  (S1 ++ S2).terms = S1.terms ++ S2.terms := rfl
+theorem append_terms (P1 P2 : @pterm γ _) :
+  (P1 ++ P2).terms = P1.terms ++ P2.terms := rfl
 
-def pow (S : @pterm γ _) (n : znum) : @pterm γ _ :=
+def mul (P : @pterm γ _) (n : znum) : @pterm γ _ :=
 if n = 0 then ⟨[]⟩
-else if n = 1 then S
-else ⟨S.terms.map (λ x, x.pow n)⟩
+else if n = 1 then P
+else ⟨P.terms.map (λ x, x.mul n)⟩
 
-def eval (ρ : dict α) (S : @pterm γ _) : α :=
-list.prod (S.terms.map (xterm.eval ρ))
+def eval (ρ : dict α) (P : @pterm γ _) : α :=
+list.prod (P.terms.map (xterm.eval ρ))
 
-theorem eval_pow (S : @pterm γ _) (n : znum) :
-(S.pow n).eval ρ = S.eval ρ ^ (n : ℤ) :=
+
+theorem eval_mul (P : @pterm γ _) (n : znum) :
+(P.mul n).eval ρ = P.eval ρ ^ (n : ℤ) :=
 begin
   by_cases h : n = 0 ∨ n = 1,
   { cases h with h h,
-    { simp [h, pow, eval] },
-    { simp [h, pow, eval] }},
+    { simp [h, mul, eval] },
+    { simp [h, mul, eval] }},
   { have h0 : ¬ n = 0, by { intro h0, apply h, left, exact h0 },
     have h1 : ¬ n = 1, by { intro h1, apply h, right, exact h1 },
-    cases S with terms,
-    simp [h0, h1, pow, eval], 
+    cases P with terms,
+    simp [h0, h1, mul, eval], 
     induction terms with x xs ih,
     { simp },
-    { simp [ih, mul_fpow, xterm.eval_pow] }
+    { simp [ih, mul_fpow, xterm.eval_mul] }
   }
 end
 
-theorem eval_mul (S1 S2 : @pterm γ _) :
-  (S1 ++ S2).eval ρ = S1.eval ρ * S2.eval ρ :=
+theorem eval_append (P1 P2 : @pterm γ _) :
+  (P1 ++ P2).eval ρ = P1.eval ρ * P2.eval ρ :=
 begin
   unfold eval,
   simp only [append_terms],
@@ -108,83 +142,117 @@ begin
   apply list.prod_append 
 end
 
-def to_nterm (S : @pterm γ _) : @nterm γ _ :=
-prod (S.terms.map (xterm.to_nterm))
+def to_nterm (P : @pterm γ _) : @nterm γ _ :=
+prod (P.terms.map (xterm.to_nterm))
 
-theorem eval_to_nterm {S : @pterm γ _} :
-  S.eval ρ = S.to_nterm.eval ρ :=
+theorem eval_to_nterm {P : @pterm γ _} :
+  P.eval ρ = P.to_nterm.eval ρ :=
 begin
-  cases S with terms,
+  cases P with terms,
   unfold to_nterm, unfold eval,
   rw [eval_prod, list.map_map],
   rw xterm.eval_to_nterm'
 end
 
-def reduce_aux : @xterm γ _ → list (@xterm γ _) → list (@xterm γ _)
-| x []      := [x] --TODO
-| x (y::ys) :=
-  if x.term = y.term then
-    reduce_aux ⟨x.term, x.exp + y.exp⟩ ys
-  else if x.exp = 0 then
-    reduce_aux y ys
+structure reduce_cache : Type :=
+(t : @xterm γ _)
+(ts : list (@xterm γ _))
+(hyps : list (@nterm γ _))
+
+def reduce_cache.eval (ρ : dict α) (pi : @reduce_cache γ _) : α :=
+if nterm.eval ρ pi.t.term = 0 then 0
+else xterm.eval ρ pi.t * pterm.eval ρ ⟨pi.ts⟩
+
+def reduce_aux : @reduce_cache γ _ → @xterm γ _ → @reduce_cache γ _
+| ⟨⟨x, n⟩, ts, hyps⟩ ⟨y, m⟩ :=
+  if m = 0 then
+    ⟨⟨x, n⟩, ts, hyps⟩
+  else if x = y then
+    ⟨⟨x, n + m⟩, ts, hyps⟩
+  else if n = 0 then
+    ⟨⟨y, m⟩, ts, x::hyps⟩
   else
-    x::(reduce_aux y ys)
+    ⟨⟨y, m⟩, ⟨x, n⟩::ts, hyps⟩
+
+def reduce_ih (ρ : dict α) (c : α) (pi : @reduce_cache γ _) : Prop :=
+  (∀ h ∈ pi.hyps, nterm.eval ρ h ≠ 0) → pi.eval ρ = c
+
+theorem eval_reduce_aux (c : α) (y : @xterm γ _)
+  (pi : @reduce_cache γ _)
+  (ih : reduce_ih ρ c pi) :
+  reduce_ih ρ (xterm.eval ρ y * c) (reduce_aux pi y) :=
+begin
+  cases pi with x xs ts,
+  cases x with x n,
+  cases y with y m,
+  intro H,
+  have H' : ∀ t ∈ ts, nterm.eval ρ t ≠ 0,
+  by {
+    by_cases h1 : m = 0,
+    { simp [reduce_aux, h1] at H, apply H },
+    { by_cases h2 : x = y,
+      { simp [reduce_aux, h1, h2] at H, apply H },
+      { by_cases h3 : n = 0,
+        { simp [reduce_aux, h1, h2, h3] at H, apply H.right },
+        { simp [reduce_aux, h1, h2, h3] at H, apply H }}}},
+  have ih' := ih H',
+  have hc : nterm.eval ρ x = 0 → c = 0,
+  by { intro hx, rw ← ih H', simp [reduce_cache.eval, hx] },
   
-theorem eval_reduce_aux (x : @xterm γ _) (xs : list (@xterm γ _)) :
-  pterm.eval ρ ⟨reduce_aux x xs⟩ = pterm.eval ρ ⟨x::xs⟩ :=
-begin
-  revert x,
-  induction xs with y ys ih,
-  { simp [eval, reduce_aux] },
-  { intro x,
-    unfold eval,
-    rw [list.map_cons, list.prod_cons],
-    unfold eval at ih,
-    by_cases h1 : x.term = y.term,
-    { rw [list.map_cons, list.prod_cons],
-      rw [← mul_assoc],
-      unfold xterm.eval,
-      rw [h1, ← fpow_add],
-      { norm_cast,
-        rw [← xterm.eval_def, ← list.prod_cons, ← list.map_cons],
-        rw [← ih],
-        simp [reduce_aux, h1] },
-      { sorry }}, -- TODO: introduce hypothesis
-    by_cases h2 : x.exp = 0,
-    { rw [list.map_cons, list.prod_cons],
-      unfold xterm.eval,
-      rw_mod_cast [h2, fpow_zero],
-      rw [← xterm.eval_def, ← list.prod_cons, ← list.map_cons],
-      simp [reduce_aux, h1, h2, ih], refl
-    },
-    conv {
-      to_lhs,
-      simp [reduce_aux, h1, h2]
-    },
-    simp [ih]
-  }
+  by_cases h1 : m = 0,
+  { simp [reduce_aux, xterm.eval, h1, ih H'] },
+  by_cases hy : nterm.eval ρ y = 0,
+  { have : (0 : α) ^ (m : ℤ) = 0,
+    by { apply zero_fpow, rw ← znum.cast_zero, exact_mod_cast h1 },
+    by_cases h2 : x = y; by_cases h3 : n = 0;
+    simp [reduce_aux, reduce_cache.eval, xterm.eval, h1, h2, h3, hy, this] },
+  by_cases h2 : x = y,
+  { have hx : nterm.eval ρ x ≠ 0, by rw h2; exact hy,
+    suffices : xterm.eval ρ ⟨y, n + m⟩ * pterm.eval ρ ⟨xs⟩ = xterm.eval ρ ⟨y, m⟩ * c,
+    by simp [reduce_aux, reduce_cache.eval, h1, h2, hy, this],
+    calc
+    xterm.eval ρ ⟨y, n + m⟩ * pterm.eval ρ ⟨xs⟩
+        = xterm.eval ρ ⟨y, n⟩ * xterm.eval ρ ⟨y, m⟩ * pterm.eval ρ ⟨xs⟩ : by rw xterm.eval_add' hy
+    ... = xterm.eval ρ ⟨y, m⟩ * (xterm.eval ρ ⟨y, n⟩ * pterm.eval ρ ⟨xs⟩) : by ring
+    ... = xterm.eval ρ ⟨y, m⟩ * c : by { rw ← ih H', rw ← h2, simp [reduce_cache.eval, hx] }
+  },
+  by_cases h3 : n = 0,
+  { have h3' : (n : ℤ) = 0, by { rw ← znum.cast_zero, exact_mod_cast h3 },
+    suffices : xterm.eval ρ ⟨y, m⟩ * pterm.eval ρ ⟨xs⟩ = xterm.eval ρ ⟨y, m⟩ * c,
+    by simp [reduce_aux, reduce_cache.eval, h1, h2, h3, hy, this],
+    have hx : nterm.eval ρ x ≠ 0,
+    by { apply H, simp [reduce_aux, h1, h2, h3] },
+    simp [reduce_cache.eval, xterm.eval, hx, h3] at ih',
+    rw ih' },
+  have h3' : (n : ℤ) ≠ 0, by { rw ← znum.cast_zero, exact_mod_cast h3 },
+  by_cases hx : nterm.eval ρ x = 0,
+  { simp [reduce_aux, reduce_cache.eval, xterm.eval, pterm.eval, h1, h2, h3, hy, hx, hc, zero_fpow, h3'] },
+  simp [reduce_cache.eval, xterm.eval, hx] at ih',
+  rw ← ih',
+  simp [reduce_aux, h1, h2, h3, reduce_cache.eval, hx, xterm.eval, hy, pterm.eval]
+
 end
 
-def reduce (S : @pterm γ _) : @pterm γ _ :=
-match S.terms with
-| []      := ⟨[]⟩
-| (x::xs) := ⟨reduce_aux x xs⟩
-end
- 
-theorem eval_reduce {S : @pterm γ _} :
-  S.eval ρ = S.reduce.eval ρ :=
-begin
-  cases S with terms,
-  cases terms with x xs,
-  { simp [reduce] },
-  { simp [reduce, eval_reduce_aux] }
+def reduce (P : @pterm γ _) : list (@nterm γ _) × @pterm γ _ :=
+match P.terms with
+| []      := ([], ⟨[]⟩)
+| (x::xs) :=
+  let pi := list.foldl reduce_aux ⟨x, [], []⟩ xs in
+  if pi.t.exp = 0 then
+    (pi.t.term::pi.hyps, ⟨xs⟩)
+  else
+    (pi.hyps, ⟨x::xs⟩)
 end
 
-def sort (S : @pterm γ _) : @pterm γ _ :=
-⟨S.terms.merge_sort (≤)⟩
+theorem eval_reduce {P : @pterm γ _} :
+  (∀ t ∈ P.reduce.fst, nterm.eval ρ t ≠ 0) → P.eval ρ = P.reduce.snd.eval ρ :=
+by sorry
 
-theorem eval_sort {S : @pterm γ _} :
-  S.eval ρ = S.sort.eval ρ :=
+def sort (P : @pterm γ _) : @pterm γ _ :=
+⟨P.terms.merge_sort (≤)⟩
+
+theorem eval_sort {P : @pterm γ _} :
+  P.eval ρ = P.sort.eval ρ :=
 begin
   unfold eval, unfold sort,
   apply list.prod_eq_of_perm,
@@ -199,7 +267,7 @@ namespace nterm
 
 def to_pterm : @nterm γ _ → @pterm γ _
 | (mul x y) := x.to_pterm ++ y.to_pterm
-| (pow x n) := x.to_pterm.pow n
+| (pow x n) := x.to_pterm.mul n
 | x := ⟨[⟨x, 1⟩]⟩
 
 theorem eval_to_pterm {x : @nterm γ _} :
@@ -210,22 +278,24 @@ begin
   { simp [to_pterm, nterm.eval, pterm.eval, xterm.eval] },
   { simp [to_pterm, nterm.eval, pterm.eval, xterm.eval] },
   { unfold to_pterm, unfold eval,
-    rw [pterm.eval_mul, ihx, ihy] },
+    rw [pterm.eval_append, ihx, ihy] },
   { unfold to_pterm, unfold eval,
-    rw [pterm.eval_pow, ihx] }
+    rw [pterm.eval_mul, ihx] }
 
 end
 
-def norm (x : @nterm γ _) : @nterm γ _ :=
-x.to_pterm.sort.reduce.to_nterm
+def norm (x : @nterm γ _) : list (@nterm γ _) × @nterm γ _ :=
+let pi := x.to_pterm.sort.reduce in
+(pi.fst, pi.snd.to_nterm)
 
 theorem correctness {x : @nterm γ _} :
-  x.eval ρ = x.norm.eval ρ :=
+  (∀ t ∈ x.norm.fst, nterm.eval ρ t ≠ 0) → x.eval ρ = x.norm.snd.eval ρ :=
+assume h,
 calc
-  x.eval ρ = pterm.eval ρ x.to_pterm             : eval_to_pterm
-       ... = pterm.eval ρ x.to_pterm.sort        : pterm.eval_sort
-       ... = pterm.eval ρ x.to_pterm.sort.reduce : pterm.eval_reduce
-       ... = nterm.eval ρ x.norm                 : pterm.eval_to_nterm
+  x.eval ρ = pterm.eval ρ x.to_pterm                 : eval_to_pterm
+       ... = pterm.eval ρ x.to_pterm.sort            : pterm.eval_sort
+       ... = pterm.eval ρ x.to_pterm.sort.reduce.snd : pterm.eval_reduce h
+       ... = nterm.eval ρ x.norm.snd                 : pterm.eval_to_nterm
 
 end nterm
 
